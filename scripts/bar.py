11a8a2edb0240dcb85732c7701104440fd436451
Akkuzin Iliya <gr3yknigh1@gmail.com>
2021-10-19 23:30:07 +0300

    implement build and run script called bar.py

---
+++ scripts/bar.py
@@ -0,0 +1,47 @@
+"""
+"bar" == "Build And Run"
+This is pritty basic script that builds C++ file with g++ and puts executable to build folder.
+"""
+import os
+import argparse
+import pathlib
+
+
+# TODO: Refactor 
+class Gpp(object):
+
+	filepath: str
+	destpath: str
+
+	def __init__(self, filepath: str, destpath: str) -> None:
+		self.filepath = filepath
+		self.destpath = destpath
+
+	def build(self) -> int:
+		return os.system(f"g++ {self.filepath} -o {self.destpath}")
+
+	def run(self) -> int:
+		return os.system(self.destpath)
+
+
+def main() -> int:
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filepath")
+	parser.add_argument("--destpath")
+	args = parser.parse_args()
+
+	if args.destpath is None:
+		args.destpath = fr".\build\{pathlib.Path(args.filepath).stem}.exe"  # TODO: get filename with sys.path
+	
+	gpp = Gpp(args.filepath, args.destpath)
+	
+	build_res = gpp.build()
+	if not build_res:
+		run_res = gpp.run()
+		return run_res
+	else:
+		return build_res
+
+
+if __name__ == "__main__":
+	raise SystemExit(main())
